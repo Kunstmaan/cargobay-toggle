dd12d97964ca5a3ce37298d0891ca50ec00be826
Dries Beerten <dries.beerten@kunstmaan.be>
2015-11-09 10:54:15 +0100

    new structure

--- js/cargobay.toggle.js
+++ js/cargobay.toggle.js
@@ -1,23 +1,21 @@
 /* ==========================================================================
-   Toggle
+ Toggle
 
-   Initialize:
-   cargobay.toggle.init();
+ Initialize:
+ cargobay.toggle();
 
-   Support:
-   Latest Chrome
-   Latest FireFox
-   Latest Safari
-   IE10 and up
-   ========================================================================== */
-
+ Support:
+ Latest Chrome
+ Latest FireFox
+ Latest Safari
+ IE10 and up
+ ========================================================================== */
 
 var cargobay = cargobay || {};
 
+cargobay.toggle = function(window, undefined) {
 
-cargobay.toggle = (function(window, undefined) {
-
-    var init, findParent, addMultiEventistener, toggle, show, hide;
+    var init, _addMultiEventistener, toggle, _show, _hide, _hideFast;
 
     // Config
     var defaultAnimationDuration = 150,
@@ -28,13 +26,8 @@ cargobay.toggle = (function(window, undefined) {
         itemContentClass = 'toggle-item__content';
 
 
-    // Init
-    init = function() {
-        toggle();
-    };
-
     // Add multiple listeners
-    addMultiEventistener = function(el, s, fn) {
+    _addMultiEventistener = function(el, s, fn) {
         var evts = s.split(' ');
 
         for (var i=0, iLen=evts.length; i<iLen; i++) {
@@ -45,12 +38,12 @@ cargobay.toggle = (function(window, undefined) {
 
     // Main toggle function
     toggle = function() {
-        [].forEach.call( document.querySelectorAll('.' + btnClass), function(btn) {
-            addMultiEventistener(btn, 'click touchstart mousedown', function(e){
+        [].forEach.call(document.querySelectorAll('.' + btnClass), function(btn) {
+            _addMultiEventistener(btn, 'click touchstart mousedown', function(e){
                 e.preventDefault();
             });
 
-            addMultiEventistener(btn, 'touchend mouseup', function(e){
+            _addMultiEventistener(btn, 'touchend mouseup', function(e){
                 var target = document.querySelectorAll(btn.getAttribute('data-target'))[0],
                     targetContent = target.querySelectorAll('.' + itemContentClass)[0],
                     targetContentHeight = targetContent.offsetHeight,
@@ -62,27 +55,29 @@ cargobay.toggle = (function(window, undefined) {
 
                 if(currentTargetIsActive) {
                     // Target is active, so hide it
-                   hide(btn, target);
+                    _hide(btn, target);
 
                 } else {
 
                     // Check if the others have to be cleared.
                     if(hideOthers){
-                        var ownTarget = btn.getAttribute('data-target');
-                        var currentLevel = btn.getAttribute('data-level');
+                        var ownTarget = btn.getAttribute('data-target'),
+                            currentLevel = btn.getAttribute('data-level');
 
-                        [].forEach.call( document.querySelectorAll('.' + btnClass + '[data-level="' + currentLevel + '"]'), function(btn) {
+                        [].forEach.call(document.querySelectorAll('.' + btnClass + '[data-level="' + currentLevel + '"]'), function(btn) {
                             var btnTarget  = btn.getAttribute('data-target');
+
                             if(ownTarget !== btnTarget){
+
                                 if(btn.classList.contains(btnClassActive)){
-                                    hideFast(btn, document.querySelectorAll(btnTarget)[0]);
+                                    _hideFast(btn, document.querySelectorAll(btnTarget)[0]);
                                 }
                             }
                         });
                     }
 
                     // Update target
-                    show(this, target, targetContent, targetContentHeight);
+                    _show(btn, target, targetContent, targetContentHeight);
                 }
             });
         });
@@ -90,45 +85,23 @@ cargobay.toggle = (function(window, undefined) {
 
 
     // Show an item
-    show = function(btn, target, targetContent, height) {
+    _show = function(btn, target, targetContent, height) {
+        cargobay.general.animate(target, 'height', 'px', height, 'auto', animationDuration);
         btn.classList.add(btnClassActive);
-
-        Velocity({
-            elements: target,
-            properties: {
-                height: height
-            },
-            options: {
-                duration: animationDuration,
-                complete: function() {
-                    target.classList.add(itemClassActive);
-                    target.style.height = 'auto';
-                }
-            }
-        });
+        target.classList.add(itemClassActive);
     };
 
 
     // Hide an item
-    hide = function(btn, target) {
-        Velocity({
-            elements: target,
-            properties: {
-                height: 0
-            },
-            options: {
-                duration: animationDuration,
-                complete: function() {
-                    btn.classList.remove(btnClassActive);
-                }
-            }
-        });
-
+    _hide = function(btn, target, targetContent, height) {
+        cargobay.general.animate(target, 'height', 'px', 0, height, animationDuration);
+        btn.classList.remove(btnClassActive);
         target.classList.remove(itemClassActive);
     };
 
+
     // Quickly hide another open item on the same level when hide-others is true.
-    hideFast = function(btn, target) {
+    _hideFast = function(btn, target) {
         btn.classList.remove(btnClassActive);
 
         target.style.height = 0;
@@ -136,8 +109,8 @@ cargobay.toggle = (function(window, undefined) {
     };
 
 
-    return {
-        init: init
-    };
-
-}(window));
+    // Init
+    init = function() {
+        toggle();
+    }();
+};
